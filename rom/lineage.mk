ce7906553035197aae1557755372dbd14d06f312
skullknight963 <viktor.9630@hotmail.com>
2024-02-23 08:52:50 -0300

    targets: Give permissions to syslinks
    Change-Id: I34d1321e7da6c948876d171d4d453011967b3d37

--- rom/lineage.mk
+++ rom/lineage.mk
@@ -7,6 +7,11 @@
 # Common
 include device/motorola/targets/include/common.mk
 
+COMMON_LUNCH_CHOICES := \
+    lineage_$(DEVICE)-eng \
+    lineage_$(DEVICE)-user \
+    lineage_$(DEVICE)-userdebug
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
